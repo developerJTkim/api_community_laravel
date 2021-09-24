c798fb2f044e47177929691d4a00e1692c83b58f
dev-jtkim <jtkim@ilark.co.kr>
2021-09-24 18:27:40 +0900

    init

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    private $name = 'users';
+    private $comment = '사용자';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->name, function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->string('mobile',30)->nullable()->comment('휴대폰');
+            $table->boolean('is_black')->default(0)->comment('블랙리스트 여부');
+        });
+
+        DB::statement("ALERT TABLE". $this->name . " comment '" . $this->comment ."'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->name);
+    }
+}
